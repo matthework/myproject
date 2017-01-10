343c06c05a39d260a31bfefbc9ca6d5a28555c34
matthework <matthework@hotmail.com>
2017-01-11 03:41:10 +1300

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jri*)jegg=@h&soy7+@kiy)2lu9wcswd-vm#gs3$m22-#l#yfp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
